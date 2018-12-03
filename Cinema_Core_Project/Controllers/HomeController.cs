9853c006b89f709fa3062a2dbc31d763ac5da19d
mohamed ayman <mohameddon777@gmail.com>
2018-12-03 16:57:12 +0200

    Add project files.

---
+++ Cinema_Core_Project/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cinema_Core_Project.Models;
+
+namespace Cinema_Core_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
